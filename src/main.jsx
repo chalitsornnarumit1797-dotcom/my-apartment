4fcec6fbc17ec62c0b178d187b7eee68ab8a3be8
chalitsornnarumit1797-dotcom <chalitsornnarumit1797@gmail.com>
2026-03-26 22:39:21 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // ตรวจสอบว่ามีบรรทัดนี้เพื่อดึง App มาใช้งาน
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
